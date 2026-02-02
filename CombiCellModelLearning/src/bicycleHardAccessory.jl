d663f3375b82ea6ea5545a04871babee62490a44
xialu <stilese@uci.edu>
2026-02-02 15:49:54 -0800

    made bicycleHardAccessory.jl, modified sim_data to make variable number of outputs

---
+++ CombiCellModelLearning/src/bicycleHardAccessory.jl
@@ -0,0 +1,287 @@
+
+using CombiCellModelLearning
+using ComponentArrays # i feel like I shouldn't need this in here...
+using Optimization
+using OptimizationBBO
+using Statistics
+# using OptimizationBBO
+
+include("sim_data.jl")
+
+x_for_sim = [0.1, 0.02, 0.004, 0.0008, 0.00016, 0.000032, 0.0000064, 0.00000128, 0.000000256, 5.12e-8, 1.024e-8, 0.00, 0.1, 0.02, 0.004, 0.0008, 0.00016, 0.000032, 0.0000064, 0.00000128, 0.000000256, 5.12e-8, 1.024e-8, 0.00, 0.1, 0.02, 0.004, 0.0008, 0.00016, 0.000032, 0.0000064, 0.00000128, 0.000000256, 5.12e-8, 1.024e-8, 0.00]
+kD_for_sim = [8.0 for _ in x_for_sim]
+stdevs_for_sim = [0.005 for _ in x_for_sim]
+
+# function fw_sim(x::Vector{Float64}, kD::Vector{Float64}, p_derepresented)
+#     model = make_ModelCombiClassic()
+#     O1_00 = zeros(length(x))
+#     O2_00 = zeros(length(x))
+#     for i in 1:length(x)
+#         O1_00[i], O2_00[i] = fw(x[i], kD[i], p_derepresented, model)
+#     end
+#     return (O1_00, O2_00)
+# end
+
+params_for_sim = ComponentArray(
+    fI=0.6,
+    alpha=2e6,
+    tT=500.0,
+    g1=0.7,
+    k_on_2d=15.0,
+    kP=0.4,
+    nKP=1.8,
+    lambdaX=0.05,
+    nC=2.1,
+    XO1=0.4,
+    O1max=0.95,
+    O2max=110.0,
+    extraCD2 = 0.99,
+    extraPD1 = 40
+)
+
+
+# basically copied over from fw() in define_modelCombi_classical.jl
+
+function true_fw(x::Vector{Float64}, kD::Vector{Float64}, params)
+    # unpack params
+    fI, alpha, tT, g1, k_on_2d, kP, nKP, lambdaX, nC, XO1, O1max, O2max, extraCD2, extraPD1 = params
+    # hypothesis = CD2 affects O2max, PD1 affects O1max
+    params_00 = ComponentArray(
+        fI=fI,
+        alpha=alpha,
+        tT=tT,
+        g1=g1,
+        k_on_2d=k_on_2d,
+        kP=kP,
+        nKP=nKP,
+        lambdaX=lambdaX,
+        nC=nC,
+        XO1=XO1,
+        O1max=O1max,
+        O2max=O2max,
+    )
+
+    O1_00, O2_00 = true_fw_inside(x, kD, params_00)
+
+    params_10 = ComponentArray(
+        fI=fI,
+        alpha=alpha,
+        tT=tT,
+        g1=g1,
+        k_on_2d=k_on_2d,
+        kP=kP,
+        nKP=nKP,
+        lambdaX=lambdaX,
+        nC=nC,
+        XO1=XO1,
+        O1max=extraCD2,
+        O2max=O2max,
+    )
+
+    O1_10, O2_10 = true_fw_inside(x, kD, params_10)
+
+
+    params_01 = ComponentArray(
+        fI=fI,
+        alpha=alpha,
+        tT=tT,
+        g1=g1,
+        k_on_2d=k_on_2d,
+        kP=kP,
+        nKP=nKP,
+        lambdaX=lambdaX,
+        nC=nC,
+        XO1=XO1,
+        O1max=O1max,
+        O2max=extraPD1,
+    )
+
+    O1_01, O2_01 = true_fw_inside(x, kD, params_01)
+
+    params_11 = ComponentArray(
+        fI=fI,
+        alpha=alpha,
+        tT=tT,
+        g1=g1,
+        k_on_2d=k_on_2d,
+        kP=kP,
+        nKP=nKP,
+        lambdaX=lambdaX,
+        nC=nC,
+        XO1=XO1,
+        O1_max=extraCD2,
+        O2_max=extraPD1,
+    )
+    
+    O1_11, O2_11 = true_fw_inside(x, kD, params_11)
+
+    return O1_00, O2_00,O1_10, O2_10, O1_01, O2_01, O1_11, O2_11
+    
+    
+end
+function true_fw_inside(x::Vector{Float64}, kD::Vector{Float64}, params)
+    # unpack params
+    fI, alpha, tT, g1, k_on_2d, kP, nKP, lambdaX, nC, XO1, O1max, O2max = params
+
+    O1 = Float64[]
+    O2 = Float64[]
+
+    for (xi, kDi) in zip(x, kD)
+        CT = (1-fI) * (alpha * xi + tT + g1 * kDi / k_on_2d - sqrt((alpha * xi + tT + g1 * kDi / k_on_2d)^2 - 4 * alpha * xi * tT)) / 2
+        CN = (1 / (1 + g1 * kDi / kP))^nKP * CT
+        X = CN^nC / (lambdaX^nC + CN^nC)
+
+        O1_val = XO1 / (XO1 + X)
+        O2_val = X
+
+        O1i = O1max * O1_val 
+        O2i = O2max * O2_val
+
+        push!(O1, O1i)
+        push!(O2, O2i)
+    end
+    return (O1, O2)
+end
+
+fakeData = sim_data(x_for_sim, kD_for_sim, stdevs_for_sim, params_for_sim, true_fw)
+
+# O1_sim_data, O2_sim_data = sim_data(x_for_sim, kD_for_sim, stdevs_for_sim, params_for_sim, true_fw)
+
+# fakeData = Dict(
+#     "x" => x_for_sim,
+#     "KD" => kD_for_sim,
+#     # stdevs = stdevs_for_sim,
+#     "O1_00" => O1_sim_data,
+#     "O2_00" => O2_sim_data
+# )
+
+# now let's make a classical model and try to fit parameters to the simulated data
+# differential evolution
+
+
+# model = CombiCellModelLearning.make_ModelCombiClassic()
+# p_repr_ig = deepcopy(model.params_repr_ig)
+# # learning problem
+# learning_problem = CombiCellModelLearning.LearningProblem(
+#     data =fakeData,
+#     model= model,
+#     p_repr_lb=CombiCellModelLearning.represent(model.p_derepresented_lowerbounds, model),
+#     p_repr_ub=CombiCellModelLearning.represent(model.p_derepresented_upperbounds, model),
+#     mask = trues(length(x_for_sim)), # no mask for now
+#     loss_strategy="vanilla")
+
+# for bbo, use solve(prob, algo, maxiters, callback)
+
+# prob is an optimizationproblem
+# OptimizationProblem(objective_function, initial_guess, p = either constant prams or fixed used in objective function?? = [1,100.0]???, lb, ub)
+
+# for objective function, need to define function that takes in parameter array and returns loss (pass get_loss with reconstructed params)
+function obj_func(x, p)
+    p_repr = CombiCellModelLearning.reconstruct_learning_params_from_array(x, p_repr_ig, model) # this is where params are updated # the trick is the x is the actual params we want. 
+    # only pass through p_repr_ig for the keys
+    return CombiCellModelLearning.get_loss(p_repr; learning_problem=learning_problem)
+end
+
+# let's make this whole section a function bbo_learn(learning_problem, p_repr_ig)
+function bbo_learn(learning_problem, p_repr_ig)
+    # initial guess params array
+    classical_params_array = collect(values(copy(p_repr_ig)))
+
+    # p
+    p = [1.0, 100.0] # not used in obj_func, honestly I think i could delete this but will leave in for now and see if it changes anything later
+
+    # algo is the optimization algorithm (here bbo)
+    # maxiters is maximum iterations
+    maxiters = 30000 # reduced for testing
+    # callback is a function called at each iteration, s.t. optimzation stops if it returns true
+    config = CallbackConfig() # just stores info for callback function in fields
+    callback, loss_history = CombiCellModelLearning.create_bbo_callback_with_early_termination(
+    config, maxiters)
+
+    # create optimization problem
+    prob = Optimization.OptimizationProblem(
+        obj_func,
+        classical_params_array,
+        p;
+        lb= learning_problem.p_repr_lb,
+        ub=learning_problem.p_repr_ub)
+
+    # solve optimization problem
+    sol = solve(prob, BBO_adaptive_de_rand_1_bin(); callback=callback, maxiters=maxiters)
+    final_params_repr = CombiCellModelLearning.reconstruct_learning_params_from_array(sol.minimizer, p_repr_ig, learning_problem.model)
+    final_params_derepr = CombiCellModelLearning.derepresent_all(final_params_repr, learning_problem.model)
+
+    return final_params_derepr, loss_history
+end
+
+# final_params_derepr, loss_history = bbo_learn(learning_problem, p_repr_ig)
+
+# handle bounds violations
+
+# plot loss history
+# using Plots 
+# plot(loss_history, xlabel="Iteration", ylabel="Loss", title="BBO Loss History") # to do check plot
+# # save plot
+# savefig("bbo_loss_history_classic_noAccessory.png")
+# # print final params
+# println("Final parameters found by BBO:")
+# for (name, value) in zip(keys(final_params_derepr.p_classical), values(final_params_derepr.p_classical))
+#     println("$name: $value")
+# end
+# # save true params and final params to file
+# using JLD2
+# @save "bbo_final_params_classic_noAccessory.jld2" final_params_derepr params_for_sim
+
+
+# let's make a function to plot fit results vs data
+function plot_fit_vs_data(fakeData, fitted_params, name)
+    x_data = fakeData["x"]
+    kD_data = fakeData["KD"]
+    O1_data = fakeData["O1_00"]
+    O2_data = fakeData["O2_00"]
+    O1_fit, O2_fit = true_fw(x_data, kD_data, fitted_params)
+    p1 = scatter(x_data, O1_data, label="O1 Data", xlabel="x", ylabel="O1_00", title="O1 Fit vs Data")
+    plot!(p1, x_data, O1_fit, label="O1 Fit", color=:red)
+    p2 = scatter(x_data, O2_data, label="O2 Data", xlabel="x", ylabel="O2_00", title="O2 Fit vs Data")
+    plot!(p2, x_data, O2_fit, label="O2 Fit", color=:red)
+    plot(p1, p2, layout=(2,1))
+    savefig(name)
+end
+
+function plot_error(fakeData, fitted_params, name)
+    x_data = fakeData["x"]
+    kD_data = fakeData["KD"]
+    O1_data = fakeData["O1_00"]
+    O2_data = fakeData["O2_00"]
+    O1_fit, O2_fit = true_fw(x_data, kD_data, fitted_params)
+    O1_error = abs.(O1_data .- O1_fit)
+    O2_error = abs.(O2_data .- O2_fit)
+    p1 = scatter(x_data, O1_error, label="O1 Error", xlabel="x", ylabel="|O1_00 Data - Fit|", title="O1 Error vs x")
+    p2 = scatter(x_data, O2_error, label="O2 Error", xlabel="x", ylabel="|O2_00 Data - Fit|", title="O2 Error vs x")
+    plot(p1, p2, layout=(2,1))
+    savefig(name)
+end
+
+# metrics of success
+function compute_metrics(fakeData, fitted_params, name)
+    x_data = fakeData["x"]
+    kD_data = fakeData["KD"]
+    O1_data = fakeData["O1_00"]
+    O2_data = fakeData["O2_00"]
+    O1_fit, O2_fit = true_fw(x_data, kD_data, fitted_params)
+    rmse_O1 = sqrt(mean((O1_data .- O1_fit).^2))
+    rmse_O2 = sqrt(mean((O2_data .- O2_fit).^2))
+    metrics_dict = Dict(
+        "RMSE_O1" => rmse_O1,
+        "RMSE_O2" => rmse_O2,
+    )
+    @save name metrics_dict
+    return metrics_dict
+end
+
+# plot_fit_vs_data(fakeData, final_params_derepr, "bbo_fit_vs_data_classic_noAccessory.png")
+# plot_error(fakeData, final_params_derepr, "bbo_error_vs_x_classic_noAccessory.png")
+# metrics_dict = compute_metrics(fakeData, final_params_derepr, "bbo_metrics_classic_noAccessory.jld2")
+
+    
+
--- CombiCellModelLearning/src/sim_data.jl
+++ CombiCellModelLearning/src/sim_data.jl
@@ -4,17 +4,37 @@ function sim_data(x, kD, stdevs, params, true_fw)
     # stdevs is vector of standard deviations for fake experimental outputs
     # model_fw is function that takes (x, kD, params) and returns predicted outputs
     num_points = length(x)
-    sim_O1_00, sim_O2_00 = true_fw(x, kD, params)
-    # Add noise to simulate experimental data
-    noise1 = randn(num_points) .* stdevs
-    sim_O1_00 .= sim_O1_00 .+ noise1
-    noise2 = randn(num_points) .* stdevs
-    sim_O2_00 .= sim_O2_00 .+ noise2
+    # sim_O1_00, sim_O2_00 = true_fw(x, kD, params) # was used for no accessory
+    O1_00, O2_00, O1_10, O2_10, O1_01, O2_01, O1_11, O2_11 = true_fw(x, kD, params)
+    outputs = [O1_00, O2_00, O1_10, O2_10, O1_01, O2_01, O1_11, O2_11]
 
+    # Add noise to simulate experimental data
+    # noise1 = randn(num_points) .* stdevs
+    # sim_O1_00 .= sim_O1_00 .+ noise1
+    # noise2 = randn(num_points) .* stdevs
+    # sim_O2_00 .= sim_O2_00 .+ noise2
+
+    # add noise to all
+    for output in outputs
+        noise = randn(num_points).*stdevs
+        output += noise
+    end
     
 
+    fakeData = Dict(
+        "x" => x,
+        "KD" => kD,
+        "O1_00" => O1_00,
+        "O2_00" => O2_00,
+        "O1_10" => O1_10,
+        "O2_10" => O2_10,
+        "O1_01" => O1_01,
+        "O2_01" => O2_01,
+        "O1_11" => O1_11,
+        "O2_11" => O2_11
+    )
 
-    return sim_O1_00, sim_O2_00
+    return fakeData
 end
 
 
