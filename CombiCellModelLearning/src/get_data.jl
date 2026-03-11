7a29f9088b06799d14b34d77200d7f4afd3953aa
xialu <stilese@uci.edu>
2026-03-11 10:39:59 -0700

    subtracting minimum output_ligCond per KD

--- CombiCellModelLearning/src/get_data.jl
+++ CombiCellModelLearning/src/get_data.jl
@@ -17,7 +17,7 @@ function get_data(; save_data=false)
     df = DataFrame(CSV.File(joinpath(base_path, "data/cleanedCD69_IL8.csv")))
 
     # Oa_bc <- a = 1 or 2 (which output type), b = 0 or 1 (w or w/out CD58), c = 0 or 1 (w or w/out PDL1)
-
+    # if zeroed =
     # Extract the relevant columns
     data = Dict(
         "x" => df.x,
@@ -38,4 +38,28 @@ function get_data(; save_data=false)
 
     return data
 
+end
+
+function get_data_min0(; save_data=false)
+    data = get_data(save_data=false) # load the data without saving, since we will save the min0 version at the end of this function
+    # for each kd, for each ligand condition, subtract minimum output value across all x values for that kd and ligand condition from the output values for that kd and ligand condition
+    ligand_conds = ["00", "10", "01", "11"]
+    for ligand_cond in ligand_conds
+        for output_type in ["O1", "O2"]
+            col_name = output_type * "_" * ligand_cond
+            for kd in unique(data["KD"])
+                idx = findall(data["KD"] .== kd)
+                min_val = minimum(data[col_name][idx])
+                data[col_name][idx] .-= min_val
+            end
+        end
+    end
+ 
+
+    if save_data
+        base_path = "/home/xialu/Documents/W25/AllardRotation/CombiCellLocal/"
+        @save joinpath(base_path, "data/CombiCell_data_min0.jld2") data
+    end
+
+    return data
 end
